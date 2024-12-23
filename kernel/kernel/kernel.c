087dd9834d77d792a55bc8db3d2868a3b9767064
InfiniteVerma <vermaanant25@gmail.com>
2024-12-23 20:33:07 +0530

    attempt to read memory available from bootloader

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -14,32 +14,35 @@ void panic(const char* msg) {
 void kernel_main(multiboot_info_t* mbd, unsigned int magic) {
 
 	terminal_initialize();
-    //if(magic != MULTIBOOT_BOOTLOADER_MAGIC) {
-    //    printf("invalid magic number\n");
-    //    return;
-    //}
+    if(magic != MULTIBOOT_BOOTLOADER_MAGIC) {
+        printf("invalid magic number\n");
+        return;
+    }
 
-    //printf("before breakpoint");
-    //asm("int3");
-    //if(!(mbd->flags >> 6 & 0x1)) {
-    //    printf("invalid memory map given by GRUB bootloader");
-    //    return;
-    //}
+    if(!(mbd->flags >> 6 & 0x1)) {
+        printf("invalid memory map given by GRUB bootloader");
+        return;
+    }
 
-    //int i;
-    //printf("Going to walk memories\n");
-    //for(i = 0; i < mbd->mmap_length;
-    //        i += sizeof(multiboot_memory_map_t));
-    //{
-    //    multiboot_memory_map_t* mmmt = 
-    //        (multiboot_memory_map_t*) (mbd->mmap_addr + i);
-
-    //    printf("Start Addr: %x | Length: %x | Size: %x | Type: %d\n",
-    //        mmmt->addr, mmmt->len, mmmt->size, mmmt->type);
-    //}
-
+    int i;
 	printf("Hello, kernel World!\n");
     printf("Booting!\n");
+    printf("Going to walk memories\n");
+
+    long long size = 0;
+    for(i = 0; i < mbd->mmap_length;
+            i += sizeof(multiboot_memory_map_t))
+    {
+        multiboot_memory_map_t* mmmt = 
+            (multiboot_memory_map_t*) (mbd->mmap_addr + i);
+
+        printf("Start Addr: %d | Length: %d | Size: %d | Type: %d\n",
+            mmmt->addr, mmmt->len, mmmt->size, mmmt->type);
+
+        size += mmmt->size;
+    }
+
+    printf("Total size: %d!\n", size);
     const char* x = "ANANT";
     printf("%s\n", x);
     printf("%c\n", x[0]);
